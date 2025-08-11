e5ea3fd06518554b553b9669f07c1906e7c34a2b
Chef6441 <support@evergy.com.au>
2025-08-11 22:40:55 +1000

    Ensure newline formatting on Enter

--- dynamic-formatting.js
+++ dynamic-formatting.js
@@ -73,7 +73,7 @@
         updating = true;
         const caret = getCaret(el);
         let text = el.innerText;
-        const trailing = text.endsWith('\n\n');
+        const trailing = text.endsWith('\n');
         text = text.replace(/\n$/, '');
         const lines = text.split(/\n/);
 
@@ -99,8 +99,16 @@
         if (hidden) hidden.value = text;
       }
 
-
-    el.addEventListener('input', update);
+    el.addEventListener('input', (e) => {
+      if (e.inputType === 'deleteContentBackward' || e.inputType === 'deleteContentForward') {
+        if (hidden) {
+          const t = el.innerText.replace(/\n$/, '');
+          hidden.value = t;
+        }
+        return;
+      }
+      update();
+    });
     update();
   }
 
