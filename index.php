a77bf14685d38819da8d524e94c86d7a895522f8
classicfoo <michaelhuynh4000@gmail.com>
2025-12-02 09:57:11 +1100

    Remove native clear icon by using text input

--- index.php
+++ index.php
@@ -208,7 +208,7 @@ $tomorrowFmt = $tomorrow->format('Y-m-d');
                         <path fill="currentColor" d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
                     </svg>
                 </button>
-                <input type="search" id="task-search-input" class="search-input" placeholder="Search tasks…" aria-label="Search tasks" tabindex="-1">
+                <input type="text" id="task-search-input" class="search-input" placeholder="Search tasks…" aria-label="Search tasks" tabindex="-1" inputmode="search">
                 <button class="search-clear" type="button" id="task-search-clear" aria-label="Clear search">&times;</button>
             </div>
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#menu" aria-controls="menu">
